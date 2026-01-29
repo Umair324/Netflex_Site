4fa03396f2c74ae53d649a16f1aaa1e37ca6b3ad
Umair Rind <141990642+Umair324@users.noreply.github.com>
2026-01-29 22:51:24 +0500

    Update API request URLs with language and page parameters

--- my-netflex/src/Request.jsx
+++ my-netflex/src/Request.jsx
@@ -1,10 +1,11 @@
+
 const key = '8d125baac2a46cc9f98bdccb8eff7696';
 
 const request = {
-   upcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}`,
-   Popular: `  http://api.themoviedb.org/3/movie/popular?language=sv&api_key=${key}`,
-   trending: `https://api.themoviedb.org/3/trending/movie/week?api_key=${key}`,
-   topRated: `https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1&api_key=${key}`,
+  upcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+  popular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+  trending: `https://api.themoviedb.org/3/trending/movie/week?api_key=${key}`,
+  topRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
 };
 
 export default request;
