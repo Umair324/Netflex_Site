c141537d63f4c5ff1a26971a9af173f7a2b255bf
Umair Rind <141990642+Umair324@users.noreply.github.com>
2026-01-29 23:08:22 +0500

    Refactor Firebase config to use environment variables
    Updated Firebase configuration to use environment variables for sensitive data.

--- my-netflex/src/Firebase.js
+++ my-netflex/src/Firebase.js
@@ -1,15 +1,18 @@
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBhFZ0-OUo_0IaY4aYVDme1pIJZYi1lbIQ",
-    authDomain: "my-survey-b1e2b.firebaseapp.com",
-    projectId: "my-survey-b1e2b",
-    storageBucket: "my-survey-b1e2b.appspot.com",
-    messagingSenderId: "892393442952",
-    appId: "1:892393442952:web:86c3baad46c22bb0af593d"
-  };
-  
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
+export default app;
